70176b05166dc7f1894b7b6213f15de9d2d70edd
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-08-27 15:43:04 +0600

    added update deploy server

--- server/src/routes/jobs.route.js
+++ server/src/routes/jobs.route.js
@@ -8,8 +8,11 @@ const {
 const router = require("express").Router();
 
 router.delete("/jobs/delete/:id", deleteJob);
+
 router.patch("/jobs/update/:id", patchJob);
+
 router.get("/jobs", getJobs);
+
 router.post("/jobs", postJob);
 
 module.exports = router;
